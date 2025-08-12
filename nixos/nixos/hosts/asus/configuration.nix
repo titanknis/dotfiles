b32698d8d34ba35935cfe598c58cc886ec06966d
titanknis <titanknis@gmail.com>
2025-08-12 07:11:26 +0100

    chore: made asus host's configuration.nix consistand with hp host

--- nixos/nixos/hosts/asus/configuration.nix
+++ nixos/nixos/hosts/asus/configuration.nix
@@ -14,20 +14,18 @@
     ../../modules/boot.nix # LUKS, GRUB, and kernel settings
 
     # --- System Configuration ---
+    ../../modules/users.nix # User account definitions
     ../../modules/system.nix # General system settings
     ../../modules/networking.nix # Networking configuration (e.g., hostname, NetworkManager)
     ../../modules/services.nix # Services settings (e.g., printing)
+    ../../modules/kanata.nix # Keyboard Configuration
+
+    # --- Packages ---
     ../../modules/packages.nix # System packages and their configurations
-    ../../modules/zsh.nix # Zsh Shell configurations
-    ../../modules/users.nix # User account definitions
-    ../../modules/git.nix # Git-specific configuration
     ../../modules/nvim.nix # Text editor configuration
-
-    # --- Desktop Environment ---
-    ../../modules/hyprland.nix # Hyprland window manager configuration
-
-    # --- Keyboard Configuration ---
-    ../../modules/kanata.nix
+    ../../modules/git.nix
+    ../../modules/zsh.nix
+    ../../modules/hyprland.nix
 
     # --- Virtual Machine Configuration ---
     # ../../modules/virtualisation.nix
